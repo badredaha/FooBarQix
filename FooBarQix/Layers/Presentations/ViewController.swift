3ae0c7350bdcba23f89e7a713c52b2bc93199dab
Badre Daha Belghiti <badre.daha@gmail.com>
2019-09-04 21:36:41 +0200

    Add UI

--- FooBarQix/Layers/Presentations/ViewController.swift
+++ FooBarQix/Layers/Presentations/ViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFiled: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    let fooBarQix = FooBarQix()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        FooBarQix().compute("51")
     }
 
+    private func compute(_ string: String){
+        let computedString = fooBarQix.compute(string)
+        textView.text.append("\(computedString)\n")
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text{
+            compute(text)
+        }
+        return true
+    }
 }
 
